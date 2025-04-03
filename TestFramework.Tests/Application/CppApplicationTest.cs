9b3c40deb61a629fd155a0cb3ce1e17f9fe03888
talentinsight <aitalentinsight@gmail.com>
2025-04-03 13:36:57 -0400

    Move CppApplicationTest to Tests project and fix access modifiers

--- TestFramework.Tests/Application/CppApplicationTest.cs
+++ TestFramework.Tests/Application/CppApplicationTest.cs
@@ -1,78 +1,18 @@
 using System;
 using System.Threading.Tasks;
-using TestFramework.Core.Models;
-using TestFramework.Core.Utils;
-using TestFramework.Core.Logger;
 using NUnit.Framework;
+using TestFramework.Core.Application;
+using TestFramework.Core.Logger;
+using TestFramework.Core.Models;
 
-namespace TestFramework.Core.Application
+namespace TestFramework.Tests.Application
 {
-    /// <summary>
-    /// Base class for all C++ application tests
-    /// </summary>
-    public abstract class CppApplicationTest : TestBase
-    {
-        /// <summary>
-        /// Gets or sets the application instance
-        /// </summary>
-        public ICppApplication? Application { get; set; }
-
-        /// <summary>
-        /// Initializes a new instance of the CppApplicationTest class
-        /// </summary>
-        /// <param name="loggerType">Type of logger to use</param>
-        protected CppApplicationTest(LoggerType loggerType = LoggerType.Console) 
-            : base(loggerType)
-        {
-        }
-
-        /// <summary>
-        /// Sets up the test by initializing the C++ application
-        /// </summary>
-        protected override void Setup()
-        {
-            base.Setup();
-            
-            Logger.Log("Initializing C++ application...");
-            Application = CreateApplication();
-            
-            if (!Application.Initialize())
-            {
-                throw new ApplicationException("Failed to initialize C++ application");
-            }
-            
-            Logger.Log($"C++ application initialized successfully. Version: {Application.GetApplicationVersion()}");
-        }
-
-        /// <summary>
-        /// Tears down the test by shutting down the C++ application
-        /// </summary>
-        protected override void TearDown()
-        {
-            if (Application != null)
-            {
-                Logger.Log("Shutting down C++ application...");
-                Application.Shutdown();
-                Logger.Log("C++ application shut down successfully");
-            }
-            
-            base.TearDown();
-        }
-
-        /// <summary>
-        /// Creates an instance of the C++ application to test
-        /// </summary>
-        /// <returns>Instance of ICppApplication</returns>
-        protected abstract ICppApplication CreateApplication();
-    }
-
     [TestFixture]
     public class CppApplicationTest : TestBase
     {
         private ICppApplication _application;
 
-        [SetUp]
-        public override void Setup()
+        protected override void Setup()
         {
             base.Setup();
             _application = new CppApplication(Logger);
@@ -176,8 +116,7 @@ namespace TestFramework.Core.Application
             Assert.That(response, Is.Not.Null);
         }
 
-        [TearDown]
-        public override void TearDown()
+        protected override void TearDown()
         {
             _application?.Dispose();
             base.TearDown();
