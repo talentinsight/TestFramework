99ee059ac33cf18a89c1851a390dc70d6f4eb351
talentinsight <aitalentinsight@gmail.com>
2025-04-03 12:18:54 -0400

    Fix enum namespace conflicts by moving all enums to Models namespace

--- TestFramework.Core/Models/TestPriority.cs
+++ TestFramework.Core/Models/TestPriority.cs
@@ -1,1 +1,23 @@
- 
+namespace TestFramework.Core.Models
+{
+    /// <summary>
+    /// Represents the priority of a test
+    /// </summary>
+    public enum TestPriority
+    {
+        /// <summary>
+        /// High priority test
+        /// </summary>
+        High = 1,
+
+        /// <summary>
+        /// Medium priority test
+        /// </summary>
+        Medium = 2,
+
+        /// <summary>
+        /// Low priority test
+        /// </summary>
+        Low = 3
+    }
+} 
